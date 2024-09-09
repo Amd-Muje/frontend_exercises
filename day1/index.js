61ed9977008f420c3be0046a1ad6b724fc389537
Amd-Muje <ahmadmujahid343@gmail.com>
2024-09-09 19:20:18 +0800

    day 1

---
+++ day1/index.js
@@ -0,0 +1,19 @@
+let x = "Hello World";
+
+console.log(x);
+
+let y = "Selamat Datang";
+
+alert(y);
+
+let name = "Muje"
+let age = 20;
+let isStudent = true;
+
+console.log(` halo saya ${name} dan saya berumur ${age} adalah seorang mahasiswa = ${isStudent} `);
+
+let a = 10;
+let b = 5;
+let c = a + b ;
+
+console.log(c);
---
+++ tes/index.js
@@ -0,0 +1,3 @@
+import iden.age;
+
+console.log(age);
